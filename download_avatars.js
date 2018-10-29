dc8a907156e43c992a10900d474063ae216bc7a2
deventorum <deventorum@gmail.com>
2018-10-29 21:54:12 +0000

    Authorization token has been added to the API request

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,24 @@
 const request = require('request');
 
+//accesing authorization token
+const token = require('./secrets.js');
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-	
+  
+	// make an API request passing an authorization data to the header
+	var options = {
+		url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+		headers: {
+			'User-Agent': 'request',
+			Authorization: `token ${token.GITHUB_TOKEN}`
+		}
+	};
+  
+	request(options, function(err, res, body) {
+		cb(err, body);
+	});
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {

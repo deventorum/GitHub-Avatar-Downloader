08ed08f810abec4b821853a9cd56f154b46fdfff
deventorum <deventorum@gmail.com>
2018-10-29 22:26:36 +0000

    A helper function has been created to help downloading the images from given URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs');
 
 //accesing authorization token
 const token = require('./secrets.js');
@@ -24,8 +25,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
 getRepoContributors('jquery', 'jquery', function(err, result) {
 	let contibutorList = [];
 	result = JSON.parse(result);
+  
+	// It creates an array with URL links for avatars
 	result.forEach(element => {
 		contibutorList.push(element.avatar_url);
 	});
 	console.log(contibutorList);
 });
+
+// Helper function that downloads images based on the provided links
+function downloadImageByURL(url, filePath) {
+	request.get(url)            
+		.on('error', function (err) {                                  
+			throw err; 
+		})
+		.on('response', function (response) {                           
+			console.log('Response Status Code: ', response.statusMessage);
+			console.log('Content Type: ', response.headers['content-type']);
+			console.log('Downloading image...');
+		})
+		.pipe(fs.createWriteStream(filePath))
+		.on('finish', function() {
+			console.log('Download is completed');
+		}); 
+}
+downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', 'avatar/1.jpg');
